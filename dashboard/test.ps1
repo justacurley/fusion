f30e0c9c307c9b5aad6f372f234e6d8e898b017b
Alex Curley <curleylax@gmail.com>
2025-06-18 18:15:56 -0600

    [skip ci] move powershell scripts to fusion-app repo

--- dashboard/test.ps1
+++
@@ -1,6 +0,0 @@
-Import-Module UniversalDashboard
-$dashboard = New-UDDashboard -Title "My Dashboard" -Content {
-    New-UDPageHeader -Text "Hello from PowerShell Universal Dashboard!"
-}
-
-Start-UDDashboard -Dashboard $dashboard -Port 8080
