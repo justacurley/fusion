fac0da79d54dd26cec0f0cd04f4cdf6a750c3660
Alex Curley <curleylax@gmail.com>
2025-06-18 20:08:44 -0600

    add policy to allow interact shell to container

--- terraform-ecs/main.tf
+++ terraform-ecs/main.tf
@@ -19,6 +19,10 @@ resource "aws_iam_role_policy_attachment" "ecs_task_exec_policy" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+resource "aws_iam_role_policy_attachment" "ecs_task_exec_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
 resource "aws_iam_role_policy_attachment" "ecs_task_exec_efs_policy" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonElasticFileSystemClientFullAccess"
