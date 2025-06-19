4dca5f8ed6a3a03014502d47ff18d5982f99e4f3
Alex Curley <curleylax@gmail.com>
2025-06-18 21:00:43 -0600

    add policy allowing task to write to mounted volume

--- terraform-ecs/main.tf
+++ terraform-ecs/main.tf
@@ -27,8 +27,29 @@ resource "aws_iam_role_policy_attachment" "ecs_task_exec_efs_policy" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonElasticFileSystemClientFullAccess"
 }
+resource "aws_iam_policy" "efs_access_policy" {
+  name        = "EFSAccessPolicy"
+  description = "Allow ECS tasks to mount and write to EFS"
 
-
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "elasticfilesystem:ClientMount",
+          "elasticfilesystem:ClientWrite"
+        ],
+        Resource = data.aws_efs_file_system.psu.arn
+      }
+    ]
+  })
+}
+resource "aws_iam_policy_attachment" "attach_efs_policy" {
+  name       = "attach-efs-policy"
+  roles      = [aws_iam_rols.ecs_task_execution_role.name]
+  policy_arn = aws_iam_policy.efs_access_policy.arn
+}
 # ECS Cluster
 #trivy:ignore:AVD-AWS-0034
 resource "aws_ecs_cluster" "main" {
@@ -102,6 +123,7 @@ resource "aws_ecs_task_definition" "app" {
     }
   }
 }
+
 
 # ECS Service (to run the task)
 resource "aws_ecs_service" "app" {
